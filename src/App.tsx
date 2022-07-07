7ec9e2d6860bc7fe57436daf8de39687c6f5af0e
Rudney eduardo souza vieira <50462041+RudneyEduardo@users.noreply.github.com>
2022-07-06 22:04:41 -0300

    Upping front logic and first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
-const getData = () => {
-  
-}
-
 function App() {
+  const [ reasonsArr, setReasonsArr ] = useState<Array<Reason>>([])
+  const removeViewedReason = () => {
+    fetch('https://reasonssite.herokuapp.com/' + reasonsArr[0].Id,
+                {
+                  method: "PUT",
+                  body: JSON.stringify({visited: 'true'}),
+                  headers: {"Content-type": "application/json; charset=UTF-8"}
+                })
+  }
+
+  const getNewReason = () => {
+      removeViewedReason()
+      getReason()
+  }
+
+  const getReason = () => {
+    fetch('https://reasonssite.herokuapp.com/all')
+      .then(res =>
+        res.json()
+          .then(data => {
+            let randomNumber:number =  Math.floor(Math.random() * data.length);
+            console.log(data)
+            for(let i = 0; i < data.length; i++){
+              if(randomNumber === i){
+                setReasonsArr([
+                  { 
+                    Id: data[i]["_id"], 
+                    Reason: data[i].reason,
+                    Visited: data[i].visited
+                  }
+                ])
+                
+              }
+            }
+          }))   
+  }
+
+  useEffect(() => {
+    console.table(reasonsArr)
+  },[reasonsArr])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { reasonsArr.length === 0 ? <h2>Aperte Para Procurar Motivo</h2> : reasonsArr.map( (reason) => <p>{ reason.Reason }</p>) } 
+        <div>
+        {reasonsArr.length > 0 ? <button onClick={getNewReason}>Novo Motivo</button> : <button onClick={getReason}>Obter um Motivo</button>}
+        </div>
       </header>
     </div>
   );
